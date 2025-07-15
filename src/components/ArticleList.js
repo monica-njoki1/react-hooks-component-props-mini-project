69ad4560f49a62fe8acd13e2f2f9a32204af7c4e
monica <monica.njoki@student.moringaschool.com>
2025-07-15 13:24:19 +0300

    props-mini-project

---
+++ src/components/ArticleList.js
@@ -0,0 +1,16 @@
+import Article from "./Article";
+
+function ArticleList({posts}) {
+    return(
+        <main>
+            {posts.map((post) => (
+                <Article 
+                key= {post.id}
+                title = {post.title}
+                date = {post.date}
+                />
+            ))}
+        </main>
+    );
+}
+export default ArticleList;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return(
+        <div>
+            <header>
+                <h1>Overreacted</h1>
+            </header>
+        </div>
+    )
+}
+export default Header;
